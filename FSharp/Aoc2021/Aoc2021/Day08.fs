68cb06c291fa2e6e8887a0122c045ea27385d88e
Lucas van Oosterhout <lucasvanoosterhout@gmail.com>
2021-12-09 21:02:07 +0100

    day08

--- FSharp/Aoc2021/Aoc2021/Day08.fs
+++ FSharp/Aoc2021/Aoc2021/Day08.fs
@@ -26,144 +26,200 @@ open System
 
 
 type RawMessage =
-    { Alphabet: string []
-      Code: string [] }
+  { Alphabet: string []
+    Code: string [] }
 
 
 let overlap (l: string) (r: string) =
-    let ls = Set(l)
-    let rs = Set(r)
-    ls |> Set.isProperSuperset rs
+  let ls = Set(l)
+  let rs = Set(r)
+  ls |> Set.isProperSuperset rs
 
 let substract (l: string) (r: string) =
-    let ls = Set(l)
-    let rs = Set(r)
-    ls - rs |> Set.toArray |> String
+  let ls = Set(l)
+  let rs = Set(r)
+  ls - rs |> Set.toArray |> String
 
 
 let buildSimple (s: string) =
-    match s with
-    | s when s.Length = 2 -> Some((1, s))
-    | s when s.Length = 3 -> Some((7, s))
-    | s when s.Length = 4 -> Some((4, s))
-    | s when s.Length = 7 -> Some((8, s))
-    | _ -> None
+  match s with
+  | s when s.Length = 2 -> Some((1, s))
+  | s when s.Length = 3 -> Some((7, s))
+  | s when s.Length = 4 -> Some((4, s))
+  | s when s.Length = 7 -> Some((8, s))
+  | _ -> None
 
 let getCodeForNumber (num: int) (codex: (int * string) list) =
-    let _, s =
-        codex |> List.find (fun (v, s) -> v = num)
+  let _, s =
+    codex |> List.find (fun (v, s) -> v = num)
 
-    s
+  s
 
 
-let parseComplex (codex: (int * string) list) (code: string) =
-    let overlapFor s i =
-        let entry = getCodeForNumber i codex
-        overlap entry s
-        
-    let rec notInCodex s codex =
-        match codex with
-        | [] -> false
-        | head :: tail -> if codex |> Seq.contains head then true else notInCodex s tail   
 
-    let checkForFive s =
-        let nineSubThree =
-            substract (getCodeForNumber 9 codex) (getCodeForNumber 3 codex)
-        overlap s nineSubThree
+let lookupInCodex code (codex: (int * string) list) =
+  let code =
+    code |> Seq.sort |> Seq.toArray |> String
 
-    match code with
-    | s when (s.Length = 5 && (overlapFor s 1)) -> Some(5, s)
-    | s when (s.Length = 6 && (overlapFor s 4)) -> Some(9, s)
-    | s when (s.Length = 5 && (checkForFive s)) -> Some(5, s)
-//    | s when (s.Length = 5) && notInCodex s codex -> Some(2, s)
-//    | s when (s.Length = 6) -> Some(0, s)
-//    | s when (s.Length = 6) -> Some(6, s)
-    | _ -> None
+  try
+    let v, _ =
+      codex |> List.find (fun (v, s) -> s = code)
 
+    v
+  with
+  | :? Exception as ex ->
+    printfn "Exception! %s " (ex.Message)
+    -1
+
+
+let buildCompleteCodex (codex: (int * string) list) (code: string list) =
+  let overlapFor s i =
+    let entry = getCodeForNumber i codex
+    overlap entry s
+
+  let rec notInCodex s codex =
+    match codex with
+    | [] -> false
+    | head :: tail ->
+      if codex |> Seq.contains head then
+        true
+      else
+        notInCodex s tail
+
+  let checkForFive s codex =
+    let nineSubThree =
+      substract (getCodeForNumber 9 codex) (getCodeForNumber 3 codex)
+
+    let r = overlap nineSubThree s
+    r
+
+  let filterOutKnowns codex code =
+    let knowns = codex |> Seq.map (fun (v, s) -> s)
+
+    code
+    |> Seq.filter (fun x -> not (knowns |> (Seq.contains x)))
+
+
+  let codex =
+    (3,
+     code
+     |> Seq.find (fun s -> (s.Length = 5 && (overlapFor s 1))))
+    :: codex
+
+  let codex =
+    (9,
+     code
+     |> Seq.find (fun s -> (s.Length = 6 && (overlapFor s 4))))
+    :: codex
+
+  let code = filterOutKnowns codex code
+
+  let codex =
+    (5,
+     code
+     |> Seq.find (fun s -> (s.Length = 5 && (checkForFive s codex))))
+    :: codex
+
+  let code = filterOutKnowns codex code
+
+  let codex =
+    (2, code |> Seq.find (fun s -> s.Length = 5))
+    :: codex
+
+  let codex =
+    (0,
+     code
+     |> Seq.find (fun s -> (s.Length = 6 && (overlapFor s 1))))
+    :: codex
+
+  let code = filterOutKnowns codex code
+
+  let codex =
+    (6, code |> Seq.find (fun s -> (s.Length = 6)))
+    :: codex
+
+  Assert.Equal(codex.Length, 10)
+
+  codex
+  |> Seq.map (fun (v, s) -> (v, s |> Seq.sort |> Seq.toArray |> String))
+  |> Seq.toList
 
 let buildMessage (msg: RawMessage) =
-    let simplestNumbers input =
-        input
-        |> Seq.map (fun s -> buildSimple s)
-        |> Seq.map
-            (fun x ->
-                match x with
-                | Some (v, s) -> (v, s)
-                | None -> (-1, ""))
-        |> Seq.filter (fun (v, s) -> v <> -1)
+  let simplestNumbers input =
+    input
+    |> Seq.map (fun s -> buildSimple s)
+    |> Seq.map
+         (fun x ->
+           match x with
+           | Some (v, s) -> (v, s)
+           | None -> (-1, ""))
+    |> Seq.filter (fun (v, s) -> v <> -1)
 
-    let knowns =
-        simplestNumbers msg.Alphabet |> Seq.toList
+  let knowns =
+    simplestNumbers msg.Alphabet |> Seq.toList
 
-    // For entire list run through parseComplex, extent parsecomplex till we have no more None returning
-    // then return the codex and use is to read the input code
-    let rec doMagic codex (msg: string list) =
-        match msg with
-        | head :: tail ->
-            let r = parseComplex codex head
+  // For entire list run through parseComplex, extent parsecomplex till we have no more None returning
+  // then return the codex and use is to read the input code
 
-            match r with
-            | Some (v) -> doMagic (v :: codex) tail
-            | None -> doMagic codex (tail @ [ head ])
-        | [] -> codex
+  let codex =
+    buildCompleteCodex knowns (msg.Alphabet |> Seq.toList)
+    |> Seq.toList
 
-    let a = knowns |> Seq.map (fun (v, s) -> s)
+  let msg =
+    msg.Code
+    |> Seq.map (fun s -> lookupInCodex s codex)
+    |> Seq.map (fun v -> v.ToString())
+    |> String.Concat
+    |> Convert.ToInt32
 
-    let unknowns =
-        msg.Alphabet
-        |> Seq.filter (fun x -> a |> Seq.contains x)
-
-    let codex =
-        doMagic knowns (msg.Alphabet |> Seq.toList)
-
-    0
+  msg
 
 
 let readNumbers (input: RawMessage list) =
-    let knwonNumbers =
-        input |> Seq.map buildMessage |> Seq.toList
+  let knwonNumbers =
+    input |> Seq.map buildMessage |> Seq.toList
 
-    [ 0 ]
+  knwonNumbers
 
 let countSimpleNumbers (input: string [] list) =
 
 
-    let checkString input =
-        input
-        |> Seq.map (fun s -> buildSimple s)
-        |> Seq.sumBy
-            (fun x ->
-                match x with
-                | Some _ -> 1
-                | None -> 0)
+  let checkString input =
+    input
+    |> Seq.map (fun s -> buildSimple s)
+    |> Seq.sumBy
+         (fun x ->
+           match x with
+           | Some _ -> 1
+           | None -> 0)
 
-    let a = input |> Seq.map checkString |> Seq.sum
-    a
+  let a = input |> Seq.map checkString |> Seq.sum
+  a
 
 let createInputFromString (input: string) =
-    input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
-    |> Seq.map (fun s -> s.Split("|") |> Seq.last)
-    |> Seq.map (fun s -> s.Split(" ", StringSplitOptions.RemoveEmptyEntries))
-    |> Seq.toList
+  input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+  |> Seq.map (fun s -> s.Split("|") |> Seq.last)
+  |> Seq.map (fun s -> s.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+  |> Seq.toList
 
 let createInput2FromString (input: string) =
-    input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
-    |> Seq.map (fun s -> s.Split("|"))
-    |> Seq.map
-        (fun pair ->
-            { Alphabet =
-                  pair.[0]
-                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                  |> Seq.toArray
-              Code =
-                  pair.[1]
-                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                  |> Seq.toArray })
-    |> Seq.toList
+  input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+  |> Seq.map (fun s -> s.Split("|"))
+  |> Seq.map
+       (fun pair ->
+         { Alphabet =
+             pair.[0]
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+             |> Seq.toArray
+           Code =
+             pair.[1]
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+             |> Seq.toArray })
+  |> Seq.toList
 
 let test =
-    let input =
-        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  let input =
+    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -174,22 +230,36 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
 
-    let input1 = createInputFromString input
-    let a = countSimpleNumbers input1
+  let input1 = createInputFromString input
+  let a = countSimpleNumbers input1
 
-    let b =
-        readNumbers (createInput2FromString input)
-        |> Seq.sum
+  let known =
+    "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
 
-    Assert.Equal(26, a)
-    Assert.Equal(61229, b)
-    0
+  let t =
+    readNumbers (createInput2FromString known)
+    |> Seq.sum
+
+  Assert.Equal(5353, t)
+
+
+  let b =
+    readNumbers (createInput2FromString input)
+    |> Seq.sum
+
+  Assert.Equal(26, a)
+  Assert.Equal(61229, b)
+  0
 
 let day08 =
-    test
+  test
 
-    let input = File.ReadAllText "input/day08.txt"
-    let input = createInputFromString input
-    let a = countSimpleNumbers input
+  let inputStr  = File.ReadAllText "input/day08.txt"
+  let input = createInputFromString inputStr
+  let a = countSimpleNumbers input
 
-    (a, 0)
+  let t =
+    readNumbers (createInput2FromString inputStr)
+    |> Seq.sum
+
+  (a, t)
